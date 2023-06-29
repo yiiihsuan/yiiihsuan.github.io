b63aa442e0c275d554557c58613ea9b5561a0f89
yiiihsuan <yiiihsuann@gmail.com>
2023-06-30 03:03:57 +0800

    modified 2

--- remote-assignments/Week-1/Assignment-2/Function and Array.js
+++ remote-assignments/Week-1/Assignment-2/Function and Array.js
@@ -2,8 +2,8 @@
 function max(numbers) {
   // your code here, for-loop method preferred
   let maxnum = numbers[0];
-  for (let i = 1 ; i<numbers.length; i++){
-    if (numbers[i] > maxnum){
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxnum) {
       maxnum = numbers[i];
     }
   }
@@ -12,16 +12,16 @@ function max(numbers) {
 
 function findPosition(numbers, target) {
   // your code here, for-loop method preferred
-  for (let i = 0 ; i < numbers.length; i++){
-    if (numbers[i] === target){
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === target) {
       return i
     }
   }
   return -1;
 }
-  console.log(max([1, 2, 4, 5])); // should print 5
-  console.log(max([5, 2, 7, 1, 6])); // should print 7
-  console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
-  console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
-  console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); // should print 2 (the first position)
-  console.log(findPosition([5, 2, 7, 1, 6], 8)); // should print -1
+console.log(max([1, 2, 4, 5])); // should print 5
+console.log(max([5, 2, 7, 1, 6])); // should print 7
+console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
+console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
+console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); // should print 2 (the first position)
+console.log(findPosition([5, 2, 7, 1, 6], 8)); // should print -1
