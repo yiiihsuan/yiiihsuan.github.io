8c57fc88a04d883e4a529415e7417b643ab34416
yiiihsuan <yiiihsuann@gmail.com>
2023-06-30 11:08:39 +0800

    3

--- remote-assignments/Week-1/Assignment-3/Callback Function.js
+++ remote-assignments/Week-1/Assignment-3/Callback Function.js
@@ -12,5 +12,5 @@ function delayedResult(n1, n2, delayTime, callback) {
   
   delayedResult(-5, 10, 2000, function(res){
     console.log(res);
-  })
+  });
   // Output: 5 (-5+10) will be shown in the console after 2 seconds
