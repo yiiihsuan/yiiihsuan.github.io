eaffc12f0727d4a9fdeeee96fa28860281503c1b
yiiihsuan <yiiihsuann@gmail.com>
2023-06-30 11:05:17 +0800

    func

--- remote-assignments/Week-1/Assignment-6/promise.js
+++ remote-assignments/Week-1/Assignment-6/promise.js
@@ -1,11 +1,12 @@
+
 function delayedResultPromise(n1, n2, delayTime) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
+  return new Promise( function(resolve, reject)  {
+    setTimeout( function() {
       resolve(n1 + n2);
     }, delayTime);
   });
 }
 
-delayedResultPromise(4, 5, 3000).then(console.log);
+delayedResultPromise(4, 5, 3000).then(resolve => console.log(resolve));
 // Output: 9 (4 + 5) will be shown in the console after 3 seconds
 
